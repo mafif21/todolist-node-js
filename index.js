b6b47646110a6a5fcb10e6acc6f66a65192cebf6
unknown <afif.maliki21@gmail.com>
2023-02-10 16:24:22 +0700

    Setting ejs layout

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // import all depedencies
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressEjsLayouts = require("express-ejs-layouts");
 
 // initialization
 const app = express();
 const port = 3000;
 
 // setup
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { layout: "layouts/main" });
+});
 
 app.listen(port, () => console.log("Connected"));
